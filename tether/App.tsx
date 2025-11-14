786908b9cad6feff9963872c9f07144ecce31c08
yuinaiseki <isekiyui@grinnell.edu>
2025-11-14 15:03:34 -0800

    assets + vanilla framework

--- tether/App.tsx
+++ tether/App.tsx
@@ -1,14 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import styles from './styles/styles';
+
 export default function App() {
+  const [screen, setScreen] = useState('title');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TetherProvider>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.container}>
+        {screen === 'title' && <TitleScreen onNext={() => setScreen('contacts')} />}
+        {screen === 'contacts' && <ContactsScreen onNext={() => setScreen('message')} onBack={() => setScreen('title')} />}
+        {screen === 'message' && <MessageScreen onNext={() => setScreen('home')} onBack={() => setScreen('contacts')} />}
+        {screen === 'home' && <HomeScreen onBack={() => setScreen('contacts')} />}
+      </SafeAreaView>
+    </TetherProvider>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
